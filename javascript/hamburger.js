5fb05b7499bac825913b0def77e3f15c53f5b4c8
Gid <gid@Gids-iMac.local>
2023-03-10 18:52:53 +0000

    projectCarousel

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -2,6 +2,23 @@ export function hamburgerMenu() {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
     const navLink = document.querySelectorAll(".nav-item");
+    const viewportWidth = window.innerWidth;
+    const navFlag = document.querySelector('.projectCarousel');
+
+    if (viewportWidth < 576) {
+        navFlag.href = '#carousel';
+    } else {
+        navFlag.href = '#projects';
+    }
+
+    window.addEventListener('resize', () => {
+        const updatedViewportWidth = window.innerWidth;
+        if (updatedViewportWidth < 576) {
+            navFlag.href = '#carousel';
+        } else {
+            navFlag.href = '#projects';
+        }
+    });
 
     const mobileMenu = function() {
         hamburger.classList.toggle("active");
